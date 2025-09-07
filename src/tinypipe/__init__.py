766b8e97676dd86e72d06dc4eb3390b99a6abd1b
Andrew Ruba <andrewruba@Andrews-MacBook-Pro.local>
2025-09-07 11:23:41 -0400

    centralize version

--- src/tinypipe/__init__.py
+++ src/tinypipe/__init__.py
@@ -2,6 +2,16 @@ from .step import Step, pipestep
 from .engine import Pipeline
 from .models import PipelineContext
 from .enums import PipelineSignal
+from importlib.metadata import version
 
-__version__ = "0.1.0"
-__all__ = ["Step", "pipestep", "Pipeline", "PipelineContext", "PipelineSignal"]
+
+__version__ = version("tinypipe")
+
+__all__ = [
+    "__version__",
+    "Step",
+    "pipestep",
+    "Pipeline",
+    "PipelineContext",
+    "PipelineSignal",
+]
